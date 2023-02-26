d293366914f57970266911d16d0f1a8bbf858295
James Haughey <jameshaughey@github.com>
2023-02-26 23:28:05 +0000

    Fix iam policy

--- 10_iam/main.tf
+++ 10_iam/main.tf
@@ -1,3 +1,17 @@
+data "aws_iam_policy_document" "base" {
+  statement {
+    sid = "BaseAccess"
+
+    actions = [
+      "s3:ListBucket",
+      "s3:ListBucketVersions"
+    ]
+
+    resources = ["arn:aws:s3:::*"]
+    effect    = "Allow"
+  }
+}
+
 module "iam_role" {
   source  = "cloudposse/iam-role/aws"
   version = "0.17.0"
@@ -6,6 +20,10 @@ module "iam_role" {
   principals = {
     "Service": ["lambda.amazonaws.com"]
   }
+  policy_description = "S3 List Access"
+  policy_documents = [
+    data.aws_iam_policy_document.base.json
+  ]
 
   context = module.this.context
 }
